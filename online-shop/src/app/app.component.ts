5a537b7ef7e24b347ae396734711f4bad9dda239
Maksim Samuilionak <maximwow70@gmail.com>
2017-06-04 14:10:11 +0300

    test http

--- online-shop/src/app/app.component.ts
+++ online-shop/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
+
+	private _responseFromServer = 'pesos';
+
+	constructor(private _http: Http) {
+		this.getResponseFromServer();
+	}
+
+	private getResponseFromServer(): void {
+		this._http.get('TestServlet')
+			.map(response => response.json()).subscribe(
+				response => this._responseFromServer = response.toString()
+			);
+	}
+
+	public get responseFromServer(): string {
+		return this._responseFromServer;
+	}
 }
