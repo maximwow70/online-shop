b85501d194f82fa02bc8d553a15e07a66cb22636
Maksim Samuilionak <maximwow70@gmail.com>
2017-06-18 17:10:53 +0300

    online-shop 0.1.6

--- online-shop/src/app/item-liker/item-liker.component.ts
+++ online-shop/src/app/item-liker/item-liker.component.ts
@@ -3,6 +3,7 @@ import { ItemDataPresentation } from "app/_model/item-data-presentation";
 import { ItemListService } from "app/_services/item-list/item-list.service";
 import { ItemColorService } from "app/_services/item-color/item-color.service";
 import { Color } from "app/_model/color";
+import { Item } from "app/_model/item";
 
 @Component({
 	selector: 'item-liker',
@@ -14,33 +15,74 @@ export class ItemLikerComponent implements OnInit {
 
 	private _itemList: ItemDataPresentation[] = [];
 
+	private _itemListLiked: ItemDataPresentation[] = [];
+	private _itemListDisliked: ItemDataPresentation[] = [];
+
+	private _listLength: number;
 	constructor(
 		private _itemListData: ItemListService,
 		private _itemColor: ItemColorService
 	) {
-
+		
 	}
 
 	ngOnInit() {
+		this.getItemList();
+	}
+
+
+	public onItemLiked(): void {
+		if (this._itemList.length > 0) {
+			let likedItem = this._itemList.pop();
+			this._itemListLiked.push(likedItem);
+		}
+		if (this._itemListLiked.length > this._listLength){
+			this._itemListLiked.shift();
+		}
+	}
+	public onItemDisliked(): void {
+		if (this._itemList.length > 0) {
+			let dislikedItem = this._itemList.pop();
+			this._itemListDisliked.push(dislikedItem);
+		}
+		if (this._itemListDisliked.length > this._listLength){
+			this._itemListDisliked.shift();
+		}
+	}
+	public onRefresh(): void {
+		this.getItemList();
+	}
+
+	public getItemList(): void {
 		let that = this;
 
 		this._itemListData.getItemList().subscribe(
 			itemDataList => {
 				let itemList = [];
-				for (let i = 0; i < itemDataList.length; i++){
+				for (let i = 0; i < itemDataList.length; i++) {
 					itemList.push(ItemDataPresentation.fromObject(itemDataList[i]));
 				}
 				that._itemList = itemList;
+
+				that._listLength = that._itemList.length;
 			}
 		);
 	}
-
 	public getClassByColor(color: Color): string {
 		return this._itemColor.getClassByColor(color);
 	}
 
 	public get itemListData(): ItemDataPresentation[] {
 		return this._itemList;
+	}
+	public get itemListLiked(): ItemDataPresentation[] {
+		return this._itemListLiked;
+	}
+	public get itemListDisliked(): ItemDataPresentation[] {
+		return this._itemListDisliked;
+	}
+	public get canLiked(): boolean {
+		return this._itemList.length > 0;
 	}
 
 }
