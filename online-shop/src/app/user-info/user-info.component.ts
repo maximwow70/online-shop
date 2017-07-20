a13d97c3990ba8ff877b2eb281b8051fd2a4ec56
Oleg Vasilyev <Aleh_Vasilyeu1@epam.com>
2017-07-20 14:37:41 +0300

    UserDashboard 0.0.5

--- online-shop/src/app/user-info/user-info.component.ts
+++ online-shop/src/app/user-info/user-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDataService } from "app/_services/user-data/user-data.service";
 import { Router } from "@angular/router";
+import { User } from "app/_model/user";
 
 @Component({
 	selector: 'user-info',
@@ -20,6 +21,10 @@ export class UserInfoComponent implements OnInit {
 	public logOut(): void {
 		this._userData.logOut();
 		this._router.navigate(['user/login']);
+	}
+
+	public get user(): User {
+		return this._userData.user;
 	}
 
 }
