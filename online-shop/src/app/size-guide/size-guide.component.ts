0137f00707aa197bec084eaac24783d633c743f4
Maksim Samuilionak <maximwow70@gmail.com>
2017-08-04 22:03:36 +0300

    SizeGuide 0.0.2

--- online-shop/src/app/size-guide/size-guide.component.ts
+++ online-shop/src/app/size-guide/size-guide.component.ts
@@ -1,5 +1,25 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 
+enum ItemCategory {
+	SHOES = 0
+}
+
+const shoesSizeGuide = [
+	{
+		name: 'uk',
+		sizes: [37, 38, 39, 40, 41, 42, 43]
+	},
+	{
+		name: 'eu',
+		sizes: [3, 4, 5, 6, 7, 8, 9]
+	},
+	{
+		name: 'aus',
+		sizes: [5, 6, 7, 8, 9, 10, 11]
+	}
+]
+
+
 @Component({
 	selector: 'size-guide',
 	templateUrl: './size-guide.component.html',
@@ -10,6 +30,13 @@ export class SizeGuideComponent implements OnInit {
 	private onClick = (e) => {
 		if (!this._elementRef.nativeElement.contains((e as any).target)) {
 			this._elementRef.nativeElement.querySelector('.size_guide').classList.remove('size_guide--open');
+		}
+	}
+
+	public get vm(): any {
+		return {
+			title: 'Shoes sizes:',
+			sizeGuide: shoesSizeGuide
 		}
 	}
 
@@ -25,5 +52,7 @@ export class SizeGuideComponent implements OnInit {
 	ngOnDestroy() {
 		window.removeEventListener('click', this.onClick); 
 	}
+
+
 
 }
