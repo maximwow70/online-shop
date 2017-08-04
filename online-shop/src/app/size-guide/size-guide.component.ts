b4444305b559c540577f43de05f6f3e7c4722547
Oleg Vasilyev <Aleh_Vasilyeu1@epam.com>
2017-08-04 12:54:13 +0300

    SizeGuide 0.0.1

--- online-shop/src/app/size-guide/size-guide.component.ts
+++ online-shop/src/app/size-guide/size-guide.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'size-guide',
-  templateUrl: './size-guide.component.html',
-  styleUrls: ['./size-guide.component.scss']
+	selector: 'size-guide',
+	templateUrl: './size-guide.component.html',
+	styleUrls: ['./size-guide.component.scss']
 })
 export class SizeGuideComponent implements OnInit {
 
-  constructor() { }
+	private onClick = (e) => {
+		if (!this._elementRef.nativeElement.contains((e as any).target)) {
+			this._elementRef.nativeElement.querySelector('.size_guide').classList.remove('size_guide--open');
+		}
+	}
 
-  ngOnInit() {
-  }
+	constructor(
+		private _elementRef: ElementRef
+	) { }
+
+	ngOnInit() {
+	}
+	ngAfterViewInit() {
+		window.addEventListener('click', this.onClick);
+	}
+	ngOnDestroy() {
+		window.removeEventListener('click', this.onClick); 
+	}
 
 }
