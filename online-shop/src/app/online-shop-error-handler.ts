cff33d3c569384104264de3e41fdbe649f91b386
Maksim Samuilionak <maksim_samuilionak@epam.com>
2017-09-15 14:26:40 +0300

    Error page: provide error handling.

--- online-shop/src/app/online-shop-error-handler.ts
+++ online-shop/src/app/online-shop-error-handler.ts
@@ -6,24 +6,16 @@ import { Router } from "@angular/router";
 @Injectable()
 export class OnlineShopErrorHandler implements ErrorHandler {
 
-    private _router: Router = null;
     private _errorSubject: Subject<any> = new Subject<any>();
 
     public get errorObservable(): Observable<any> {
         return this._errorSubject;
     }
 
-    constructor(
-        private injector: Injector
-    ) {
-        this._router = this.injector.get(Router);
-        console.log(this._router);
-    }
+    constructor() { }
 
     public handleError(error: any): void {
-		this._router.navigate(['/error']);
         this._errorSubject.next(error);
-        console.error(error);
     }
 
 }
