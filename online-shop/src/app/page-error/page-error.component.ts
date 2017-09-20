3442c0b38a1bfa41b8a9aff713b3717b46fe5fb3
Maksim Samuilionak <maksim_samuilionak@epam.com>
2017-09-20 15:36:10 +0300

    Update error handling.

--- online-shop/src/app/page-error/page-error.component.ts
+++ online-shop/src/app/page-error/page-error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'page-error',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.goToHome();
+  }
+
+  public goToHome(): void {
+    this._router.navigate(['/home'])
+      .then(() => {
+        window.location.reload();
+      });
   }
 
 }
